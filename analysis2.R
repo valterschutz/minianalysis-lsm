1914486988b35ea61020139f3cb1611701e9b1c1
Valter Schütz <valterschutz@protonmail.com>
2022-11-23 14:10:00 +0100

    stuff

--- analysis2.R
+++ analysis2.R
@@ -2,36 +2,46 @@ library(chron)
 library(ggplot2)
 library(GGally)
 
-bikedata = read.csv("bikesharing.csv")
-bikedata$timestamp <- as.chron(bikedata$timestamp)
-bikedata$hrs <- hours(bikedata$timestamp)
-bikedata$hrs_cat <- factor(bikedata$hrs)
-is_rushhour <- (bikedata$hrs >= 6 & bikedata$hrs <= 9) | (bikedata$hrs >= 16 & bikedata$hrs <= 19)
-bikedata$is_rushhour <- factor(is_rushhour, levels=c(FALSE, TRUE), labels=c("Not rushhour", "Rushhour"))
-bikedata$season <- factor(bikedata$season, levels=c(0,1,2,3), labels=c("Spring","Summer","Fall","Winter"))
-bikedata$weather_code <- factor(bikedata$weather_code, levels=c(1,2,3,4,7,10,26,94), labels=c("Clear","Scattered clouds","Broken clouds","Cloudy","Rain","Rain with thunderstorm","Snowfall","Freezing fog"))
-is_holiday_or_weekend <- bikedata$is_holiday | bikedata$is_weekend
-bikedata$is_holiday_or_weekend <- factor(is_holiday_or_weekend, levels=c(FALSE,TRUE), labels=c("Not holiday/weekend","Holiday/weekend"))
+bd = read.csv("bikesharing.csv")
+bd$timestamp <- as.chron(bd$timestamp)
+bd$hrs <- hours(bd$timestamp)
+bd$hrs_cat <- factor(bd$hrs)
+bd$time_of_day = bd$hrs_cat
+levels(bd$time_of_day)[levels(bd$time_of_day)%in%c("23", "0", "1", "2", "3", "4", "5","6")] <- "Night"
+levels(bd$time_of_day)[levels(bd$time_of_day)%in%c("7", "8", "9")] <- "Morning"
+levels(bd$time_of_day)[levels(bd$time_of_day)%in%c("10", "11", "12", "13")] <- "Forenoon"
+levels(bd$time_of_day)[levels(bd$time_of_day)%in%c("14", "15", "16", "17")] <- "Afternoon"
+levels(bd$time_of_day)[levels(bd$time_of_day)%in%c("18", "19", "20", "21", "22")] <- "Evening"
+bd$time_of_day <- relevel(bd$time_of_day, ref="Night")
+is_rushhour <- (bd$hrs >= 6 & bd$hrs <= 9) | (bd$hrs >= 16 & bd$hrs <= 19)
+bd$is_rushhour <- factor(is_rushhour, levels=c(FALSE, TRUE), labels=c("Not rushhour", "Rushhour"))
+bd$season <- factor(bd$season, levels=c(0,1,2,3), labels=c("Spring","Summer","Fall","Winter"))
+bd$weather_code <- factor(bd$weather_code, levels=c(1,2,3,4,7,10,26,94), labels=c("Clear","Scattered clouds","Broken clouds","Cloudy","Rain","Rain with thunderstorm","Snowfall","Freezing fog"))
+is_holiday_or_weekend <- bd$is_holiday | bd$is_weekend
+bd$is_holiday_or_weekend <- factor(is_holiday_or_weekend, levels=c(FALSE,TRUE), labels=c("Not holiday/weekend","Holiday/weekend"))
 
 # Violin plots
-ggplot(data = bikedata, aes(x = season, y = cnt)) +
+ggplot(data = bd, aes(x = season, y = cnt)) +
   geom_violin()
-ggplot(data = bikedata, aes(x = is_holiday_or_weekend, y = cnt)) +
+ggplot(data = bd, aes(x = is_holiday_or_weekend, y = cnt)) +
   geom_violin()
 
 # See that t1 and t2 are linearly dependent
-ggpairs(bikedata)
+ggpairs(bd)
 
-# During rushhours
-ggplot(bikedata[is_rushhour,], aes(x = t1, y = cnt, color = hrs_cat)) +
+# If split into time of day, not hour
+ggplot(bd, aes(x = t1, y = cnt, color = time_of_day)) +
   geom_point(alpha=0.2) +
-  facet_grid(rows = vars(is_holiday_or_weekend), cols = vars(season))
+  guides(colour = guide_legend(override.aes = list(alpha=1))) +
+  facet_grid(rows = vars(is_holiday_or_weekend), cols = vars(season)) +
+  ylim(0,6000)
 
-# During non-rushhours
-ggplot(bikedata[!is_rushhour,], aes(x = t1, y = cnt, color = hrs_cat)) +
+# If split into hour
+ggplot(bd, aes(x = t1, y = cnt, color = hrs_cat)) +
   geom_point(alpha=0.2) +
-  facet_grid(rows = vars(is_holiday_or_weekend), cols = vars(season))
-
+  guides(colour = guide_legend(override.aes = list(alpha=1))) +
+  facet_grid(rows = vars(is_holiday_or_weekend), cols = vars(season)) +
+  ylim(0,6000)
 # TODO:
 # Greedy backward search on final model, either manually or using step(data, direction="backward")
 # Split into training/testing, calculate pMSE.
