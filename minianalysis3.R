8aef905f2aa3e2339c48554e3982795f2660c6c9
Valter Schütz <valterschutz@protonmail.com>
2022-11-25 10:30:24 +0100

    Ideas for mini3

---
+++ minianalysis3.R
@@ -0,0 +1,4 @@
+# TODO:
+# Use leaps and regsubsets to calculate all possible models, calculate pMSE for each one.
+# Plot y_test against y_pred against y_test for a good model and a bad model.
+#   Add a line with slope 1.
